25fea2f75c3bf7a7a403e326415bb40652ecce1e
Rafael Santana <aguiar.rafael003@gmail.com>
2026-04-07 19:07:34 -0300

    convsersor farenheit para celsius

--- 01_sequencial/ex009_conversaodetemperatura.cpp
+++ 01_sequencial/ex009_conversaodetemperatura.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main () {
     float c, f;
     cout <<"Conversor de temperaturas - Celsius para Farenheit\n";
-    cout <<"Insira a temperatura em graus farenheit que deseja transformar em farenheit:\n";
+    cout <<"Insira a temperatura em graus celsius que deseja transformar em farenheit:\n";
     cin >> f;
     c = (5.0 * (f - 32.0))/9;
     cout <<"Em celsius, a temperatura sera de " << c << " graus ";
