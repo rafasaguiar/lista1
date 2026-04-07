24dadb8b9f22fc1218a3e476c261d9246eeae2e4
Rafael Santana <aguiar.rafael003@gmail.com>
2026-04-07 18:03:03 -0300

    calculadora de area circular

--- 01_sequencial/ex006_circulo.cpp
+++ 01_sequencial/ex006_circulo.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+using namespace std;
 int main () {
     float raio, area;
-    std::cout <<"Calculadora de areas circulares\n";
-    std::cout <<"Insira aqui o raio, em metros, do circulo: \n";
-    std::cin >> raio;
+    cout <<"Calculadora de areas circulares\n";
+    cout <<"Insira aqui o raio, em metros, do circulo: \n";
+    cin >> raio;
     area = raio * raio * 3.14;
-    std::cout <<"A area do circulo sera de, aproximadamente, " << area << " metros quadrados. " << std::endl;
+    cout <<"A area do circulo sera de, aproximadamente, " << area << " metros quadrados. " << endl;
     return 0;
 }
