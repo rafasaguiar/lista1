1e73ab4d1d30c666f70224355a3e870f4e307df2
Rafael Santana <aguiar.rafael003@gmail.com>
2026-04-07 17:13:43 -0300

    leitura de numero informado

---
+++ 01_sequencial/ex002_numinformado.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main () {
+    int numero;
+    std::cout <<"Escolha um numero ";
+    std::cin >> numero;
+    std::cout <<"O numero informado foi " << numero;
+    return 0;
+}
