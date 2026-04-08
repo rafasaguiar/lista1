cfc61bbe86902f60fec7399274231a44ad5d25b5
Rafael Santana <aguiar.rafael003@gmail.com>
2026-04-08 17:32:22 -0300

    verificador de maior numero

---
+++ 02_selecao/ex019_maiornumero.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main () {
+    float n1, n2, n3;
+    cout <<"Digite aqui um numero real qualquer: ";
+    cin >> n1;
+    cout <<"Digite aqui outro numero real qualquer: ";
+    cin >> n2;
+    cout <<"Digite mais um numero real qualquer: ";
+    cin >> n3;
+
+    if (n1 > n2 && n1 > n3) {
+        cout <<"O maior numero e " << n1;
+    }
+    else if (n2 > n1 && n2 > n3) {
+        cout <<"O maior numero e " << n2;
+    }
+    else {
+        cout <<"O maior numero e " << n3;
+    }
+
+return 0;    
+}
