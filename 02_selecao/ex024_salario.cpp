b73930c8f4b21cc3b7a5db14df84b0ed73618f6d
Rafael Santana <aguiar.rafael003@gmail.com>
2026-04-08 19:55:07 -0300

    calculadora de reajuste salarial

---
+++ 02_selecao/ex024_salario.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+int main () {
+    float salarioatual, percentual, aumento, salarionovo; 
+    cout <<"Calculadora de reajuste salarial\n";
+    cout <<"Digite aqui o seu salario atual: \n";
+    cin >> salarioatual;
+
+    if (salarioatual <= 1280.00) {
+        salarionovo = salarioatual * 1.2;
+        aumento = salarionovo - salarioatual;
+        cout <<"\nSeu salario era de: " << salarioatual << " reais ";
+        cout <<"\nO percentual de reajuste foi de: 20%";
+        cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
+        cout <<"\nSeu novo salario sera de: " << salarionovo << " reais ";
+    }
+    
+    else if (salarioatual > 1280.00 && salarioatual <= 1700.00) {
+       salarionovo = salarioatual * 1.15;
+        aumento = salarionovo - salarioatual;
+        cout <<"\nSeu salario era de: " << salarioatual << " reais ";
+        cout <<"\nO percentual de reajuste foi de: 15%";
+        cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
+        cout <<"\nSeu novo salario sera de: " << salarionovo << " reais "; 
+    }
+
+    else if (salarioatual > 1700.00 && salarioatual <= 2500.00) {
+        salarionovo = salarioatual * 1.1;
+        aumento = salarionovo - salarioatual;
+        cout <<"\nSeu salario era de: " << salarioatual << " reais ";
+        cout <<"\nO percentual de reajuste foi de: 10%";
+        cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
+        cout <<"\nSeu novo salario sera de: " << salarionovo << " reais ";
+    }
+
+    else {
+        salarionovo = salarioatual * 1.05;
+        aumento = salarionovo - salarioatual;
+        cout <<"\nSeu salario era de: " << salarioatual << " reais ";
+        cout <<"\nO percentual de reajuste foi de: 5%";
+        cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
+        cout <<"\nSeu novo salario sera de: " << salarionovo << " reais ";
+    }
+
+return 0;    
+}
