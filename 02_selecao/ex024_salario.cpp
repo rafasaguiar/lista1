f05fab1b07fa6eda878e95b4e7ee61fd1a652c54
Rafael Santana <aguiar.rafael003@gmail.com>
2026-04-09 16:07:57 -0300

    calculo de rejuste salarial com blindagem para respostas fora de padrao

--- 02_selecao/ex024_salario.cpp
+++ 02_selecao/ex024_salario.cpp
@@ -5,8 +5,7 @@ int main () {
     cout <<"Calculadora de reajuste salarial\n";
     cout <<"Digite aqui o seu salario atual: \n";
     cin >> salarioatual;
-
-    if (salarioatual <= 1280.00) {
+    if (salarioatual <= 1280.00 && salarioatual >= 0) {
         salarionovo = salarioatual * 1.2;
         aumento = salarionovo - salarioatual;
         cout <<"\nSeu salario era de: " << salarioatual << " reais ";
@@ -14,7 +13,6 @@ int main () {
         cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
         cout <<"\nSeu novo salario sera de: " << salarionovo << " reais ";
     }
-    
     else if (salarioatual > 1280.00 && salarioatual <= 1700.00) {
        salarionovo = salarioatual * 1.15;
         aumento = salarionovo - salarioatual;
@@ -23,7 +21,6 @@ int main () {
         cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
         cout <<"\nSeu novo salario sera de: " << salarionovo << " reais "; 
     }
-
     else if (salarioatual > 1700.00 && salarioatual <= 2500.00) {
         salarionovo = salarioatual * 1.1;
         aumento = salarionovo - salarioatual;
@@ -32,8 +29,7 @@ int main () {
         cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
         cout <<"\nSeu novo salario sera de: " << salarionovo << " reais ";
     }
-
-    else {
+    else if (salarioatual > 2500.00) {
         salarionovo = salarioatual * 1.05;
         aumento = salarionovo - salarioatual;
         cout <<"\nSeu salario era de: " << salarioatual << " reais ";
@@ -41,6 +37,8 @@ int main () {
         cout <<"\nO valor do aumento foi de: " << aumento << " reais ";
         cout <<"\nSeu novo salario sera de: " << salarionovo << " reais ";
     }
-
+    else {
+        cout <<"\nValor invalido!";
+    }
 return 0;    
 }
