95fca0d1d171c2012dc4d91f694f714453951315
Rafael Santana <aguiar.rafael003@gmail.com>
2026-05-08 16:28:33 -0300

    Exercício de Fixação 2

--- lista-fixacao/fixacao2.cpp
+++ lista-fixacao/fixacao2.cpp
@@ -1,11 +1,3 @@
-// Escreva um algoritmo que calcule o preço de um produto a partir do tempo levado na sua
-// produção. Use a seguinte relação para calcular o preço:
-// 1 mês: R$ 1.000,00
-// 2 meses: R$ 1.500,00
-// 3meses: R$ 2.000,00
-// 4 meses: R$ 3.000,00
-// mais de 4 meses: R$ 5.000,00
-
 #include <iostream>
 using namespace std;
 
